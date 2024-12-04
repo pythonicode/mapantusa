f6146745f8a49ac89e29983c4f3fb417628c8a50
Anthony Riley <anthony.j.riley@gmail.com>
2024-12-03 22:17:06 -0500

    init and so it begins (pt 2)

--- components/openlayer/map.tsx
+++ components/openlayer/map.tsx
@@ -1,36 +1,29 @@
 'use client';
 
 import { View, Map } from "ol";
-import { useEffect, useRef } from "react";
+import { RefObject, useEffect, useRef } from "react";
 import OSM from "ol/source/OSM";
 import TileLayer from "ol/layer/Tile";
 import Link from "next/link";
 
-export function MapView({ zoom = 1 }: { zoom?: number }) {
+export function MapView() {
     const ref = useRef<HTMLDivElement>(null);
-    const mapRef = useRef<Map>(null);
     useEffect(() => {
-        if (ref.current && !mapRef.current) {
-            const map = new Map({
+        if (ref.current) {
+            new Map({
                 layers: [new TileLayer({
                     source: new OSM({
-
                     })
                 })],
                 view: new View({ center: [0, 0], zoom: 1 }),
                 target: ref.current
             });
-            (mapRef as any).current = map;
         }
-    }, [ref, mapRef]);
+    }, [ref]);
 
-    useEffect(() => {
-        mapRef.current?.getView().setZoom(zoom);
-    }, [mapRef, zoom]);
-
-    return (<div ref={ref} className="w-full h-screen">
+    return (<><div ref={ref} className="w-full h-screen" />
         <Link href='https://www.openstreetmap.org/copyright' className="p-1 text-xs bg-white rounded border fixed bottom-2 right-2">
             © OpenStreetMap contributors
-        </Link>
-    </div>);
+        </Link></>
+    );
 }
